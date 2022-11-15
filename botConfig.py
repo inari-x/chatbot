6be7f5dd2f21c34033362fb4d84005b23390ce6d
inari <katrinmalfent@gmail.com>
2022-11-15 16:18:02 +0100

    new style

--- botConfig.py
+++ botConfig.py
@@ -4,7 +4,7 @@
 myBotName = "Kelpie"
 botTimeZone = "America/New_York"  ##See the list full below
 botAvatar = "/static/bot.png" ##This can either be an image in your static folder or a web url to an image
-chatBG = "/static/bg5.jpg"  ##This can either be an image in your static folder or a web url to an image
+chatBG = "/static/bg3.jpg"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
 confidenceLevel = 0.65 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
 
